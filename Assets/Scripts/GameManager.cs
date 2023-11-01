afbf33ae0441a1068ea7f2cfe8a12f01b5c87b8a
netanel-magzimof <netanelmagz@gmail.com>
2023-11-01 17:45:48 +0200

    Added different platform support

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine.SocialPlatforms;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject tileTemplate; 
+    public GameObject[] tileTemplates; 
     // Start is called before the first frame update
     void Start()
     {
@@ -26,7 +26,7 @@ public class GameManager : MonoBehaviour
         {
             for (int j = -2; j < 3; j++)
             {
-                GameObject curTile = Instantiate(tileTemplate);
+                GameObject curTile = Instantiate(tileTemplates[Random.Range(0,tileTemplates.Length)]);
                 Vector3 curPos = new Vector3(i * 2.1f, height, j * 2.1f);
                 curTile.transform.position = curPos;
                 Vector3 eulerAngles = curTile.transform.eulerAngles;
