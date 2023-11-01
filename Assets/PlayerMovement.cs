d715cf5385b426fb04f16bebd61b6b43c8bf6b2e
netanel-magzimof <netanelmagz@gmail.com>
2023-11-01 23:29:32 +0200

    created player movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody _physics;
+    [SerializeField] private float dashSpeed = 50000;
+    [SerializeField] private float dashDuration = 0.001f;
+    [SerializeField] private float movementSpeed = 5;
+    [SerializeField] private float rotationSpeed = 5;
+    [SerializeField] private float dashCooldown = 1.3f;
+    
+    private bool isDuringDash, canDash;
+    
+
+    
+
+    private PlayerInputActions _playerInputActions;
+    // Start is called before the first frame update
+
+    private void FixedUpdate()
+    {
+        if (isDuringDash)
+        {
+            return;
+        }
+        
+        Vector2 inputVec = _playerInputActions.Player.Movement.ReadValue<Vector2>();
+        if (!inputVec.Equals(Vector2.zero))
+        {
+            _physics.velocity = new Vector3(inputVec.x* movementSpeed, _physics.velocity.y , inputVec.y* movementSpeed);
+            Quaternion toRotation = Quaternion.LookRotation(new Vector3(inputVec.x, 0, inputVec.y), Vector3.up);
+            transform.rotation =
+                Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.fixedTime);
+        }
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            _playerInputActions.Player.Disable();
+            _playerInputActions.UI.Enable();
+        }
+    }
+
+    void Start()
+    {
+        _physics = GetComponent<Rigidbody>();
+        _playerInputActions = new PlayerInputActions();
+        _playerInputActions.Player.Enable();
+        _playerInputActions.Player.Dash.performed += Dash;
+        _playerInputActions.UI.Submit.performed += Submit;
+        isDuringDash = false;
+        canDash = true;
+        // _playerInputActions.Player.Movement.performed += MovementPerformed;
+    }
+
+     
+    public void Dash(InputAction.CallbackContext context)
+    {
+        if (context.performed && canDash)
+        {
+            isDuringDash = true;
+            canDash = false;
+            StartCoroutine(PerformDash());
+            // _physics.AddForce(Vector3.up*jumpSpeed, ForceMode.Impulse);
+        }
+    }
+
+    private IEnumerator PerformDash()
+    {
+        float timeLeft = dashDuration;
+        // while (timeLeft > 0)
+        // {
+            Vector3 direction = transform.forward * dashSpeed;
+            // direction.y = _physics.velocity.y;
+            _physics.AddForce(direction, ForceMode.VelocityChange);
+            // timeLeft -= Time.deltaTime;
+            // yield return null;
+            
+        // }
+        yield return new WaitForSeconds(dashDuration);
+        Debug.Log("finished Dash");
+        isDuringDash = false;
+        _physics.velocity = new Vector3(0, _physics.velocity.y, 0);
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
+    }
+
+
+    public void Submit(InputAction.CallbackContext context)
+    {
+        Debug.Log("Submit");
+    }
+}
